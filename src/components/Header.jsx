b3d47c122e8171152eb65b18f1ec963c3a65c912
Durgesh Soni <durgeshsoni1082003@gmail.com>
2022-04-17 01:12:34 +0530

    master

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Style/Header.css"
+import logo from "./Media/jidologo.png"
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+      <img src={logo} alt="logo" className="logo" />
+      <nav>
+        <ul className="nav__links">
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Project</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+      <a className="cta" href="#"><button>Contact</button></a>
+    </header>
+
+    </div>
+  )
+}
+
+export default Header
