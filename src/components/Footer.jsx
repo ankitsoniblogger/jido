d7cee16c89889593a22633e5720cb7daf1d7cc8a
ankitsoniblogger <ankitsoniblogger@gmail.com>
2022-04-17 15:54:09 +0530

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Style/Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2022 - Love by India ❤</p>
+
+    </div>
+  )
+}
+
+export default Footer
